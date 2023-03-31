b7ce2b3a68c298a24d7b8230eb1c27b30ba86c12
DanyloBozhagora <danylobozhagora@gmail.com>
2023-03-31 23:24:39 +0200

    problems running main db

--- PasswordManager_backend/PasswordManager_Main/IService/IPasswordUnitService.cs
+++ PasswordManager_backend/PasswordManager_Main/IService/IPasswordUnitService.cs
@@ -4,9 +4,9 @@ namespace PasswordManager_Main.IService;
 
 public interface IPasswordUnitService
 {
-    void AddPasswordUnit(PasswordUnit passwordUnit);
-    PasswordUnit GetPasswordUnitById(int id);
-    IEnumerable<PasswordUnit> GetAllPasswordUnits();
-    void UpdatePasswordUnit(PasswordUnit passwordUnit);
+    void AddPasswordUnit(PasswordUnit passwordUnit, string masterPassword);
+    PasswordUnit GetPasswordUnitById(int id, string masterPassword);
+    IEnumerable<PasswordUnit> GetAllPasswordUnits(string masterPassword);
+    void UpdatePasswordUnit(PasswordUnit passwordUnit, string masterPassword);
     void DeletePasswordUnit(PasswordUnit passwordUnit);
 }
